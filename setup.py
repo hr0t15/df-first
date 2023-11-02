77e3cc38944bfcb9eb773b5c17793d2496eec580
hr0t15 <hr0t15.m@gmail.com>
2023-11-02 11:11:50 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='df_first',
+    version="0.1.0",
+    description="sample of minimum package",
+    author='Hiroto Minami',
+    packages=find_packages(),  # 使うモジュール一覧を指定する
+    license='MIT'
+)
